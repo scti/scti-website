41c791d0ad3739eb4067fe2a14b7aabb0224b398
rghossi <rafael.ghossi@gmail.com>
2017-07-15 23:17:32 -0300

    Add home route with hello world message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    @hello_world = 'Hello world depois de muita treta com docker, heroku e afins!'
+  end
 end
