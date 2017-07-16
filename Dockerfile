d4d0e848c0206912639c5825cb0e3d6540af8342
rghossi <rafael.ghossi@gmail.com>
2017-07-15 23:18:04 -0300

    Add setup to deploy dockerized app to heroku

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,1 @@
-FROM ruby:2.3.3
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
-RUN mkdir /scti
-WORKDIR /scti
-ADD Gemfile /scti/Gemfile
-ADD Gemfile.lock /scti/Gemfile.lock
-RUN bundle install
-ADD . /scti
+FROM heroku/ruby
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
+  # TODO: set this to true when deploying application for real
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
